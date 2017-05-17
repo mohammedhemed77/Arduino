2b5a61c5255c7f04c3d7a150359a4daf6905ca02
Mohammed Sayed hemed <mohammedhemed33@users.noreply.github.com>
2017-05-18 00:50:23 +0200

    Update increase_interrupt_sources.ino

--- increase_interrupt_sources.ino
+++ increase_interrupt_sources.ino
@@ -1,10 +1,17 @@
-/*interrupt change pin */
+/* When you want to increase No. interrupts in uC   
+  you can use interrupt change pin , there are three banks 
+  each of them contain a group of pins , if you Enable Int_change 
+  for any - or all of them , when state of any of them changed :
+  ISR will occure . 
+  So Arduino Uno interrupts = 2 + 3 = 5   
+   by : Mohammed Sayed hemed 
+*/
 const byte led = 4 ; 
 void setup() {
  pinMode (led,OUTPUT); 
  Serial.begin(9600);
- bitSet(PCICR, PCIE2); // enable pin change interrupt bank 0
- bitSet (PCMSK2, PCINT20); // enable pin change interrupt on PCINT5/D13
+ bitSet (PCICR, PCIE2);       // enable pin change interrupt bank 2
+ bitSet (PCMSK2, PCINT20);   // enable pin change interrupt on PCINT20/D4
  
 }
 
@@ -12,7 +19,7 @@ void loop() {
   digitalWrite(led,!digitalRead(4)) ; 
   delay (500) ;
   }
-
+/* Every bank has ISR 0:2 = 3 banks */
 ISR(PCINT2_vect) {
 if(digitalRead(led) == HIGH) {
 Serial.println("LED is on");
